7a12d3ab106375fe473e169fe3784fd8cfa55ae0
arunraj88 <arunrajmr88@gmail.com>
2013-03-27 09:12:41 +0530

    added cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,19 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
   validates :description, :image_url, :title , presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
-  validates :title, uniqueness: true
+  validates :title, uniqueness: true, length: { maximum: 10}
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|png)$}i,message: 'must be a URL for GIF, JPG or PNG image.' }
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        erros.add( :base, 'Line Items Present')
+        return false
+      end
+    end
 end

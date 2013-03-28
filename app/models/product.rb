8e02b4ddef788a8de4842ed0a472641b8e915f38
arunraj88 <arunrajmr88@gmail.com>
2013-03-28 09:19:20 +0530

    aaded ajax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
   has_many :line_items
+  has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :description, :image_url, :title , presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -12,7 +13,7 @@ class Product < ActiveRecord::Base
       if line_items.empty?
         return true
       else
-        erros.add( :base, 'Line Items Present')
+        errors.add( :base, 'Line Items Present')
         return false
       end
     end
